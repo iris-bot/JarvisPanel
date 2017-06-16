c5b42bcbd995e6e85f5be7af61a319a6b85768d4
Marcos Lohmann <marcos@iris-bot.com.br>
2017-06-16 20:02:11 -0300

    multiplatform with cam view

--- src/br/com/irisbot/jarvis/Main.java
+++ src/br/com/irisbot/jarvis/Main.java
@@ -1,5 +1,6 @@
 package br.com.irisbot.jarvis;
 
+import java.awt.Dimension;
 import java.util.Date;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -7,6 +8,7 @@ import java.util.logging.SimpleFormatter;
 
 import javax.swing.JFrame;
 
+import br.com.irisbot.jarvis.net.PlatformDetection;
 import br.com.irisbot.jarvis.raspi.ServiceInstance;
 import br.com.irisbot.jarvis.ui.MainPanel;
 import br.com.irisbot.ws.HttpService;
@@ -23,6 +25,8 @@ public class Main {
 			log.addHandler(fh);
 		}catch (Exception e) {}
 		
+		PlatformDetection.detectPlatform();
+		
 		HttpService srv = HttpService.intance(Port.xHTTP);
 		log.fine("HTTP SERVER READY ON PORT "+Port.xHTTP);
 		
@@ -31,9 +35,10 @@ public class Main {
 		JFrame frm = new JFrame();
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frm.setContentPane(new MainPanel());
-		frm.setAlwaysOnTop(true);
-		frm.setUndecorated(true);
-		frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frm.setMinimumSize(new Dimension(400, 400));
+		//frm.setAlwaysOnTop(true);
+		//frm.setUndecorated(true);
+		//frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frm.setVisible(true);
 		//frm.setResizable(false);
 	}
--- src/br/com/irisbot/jarvis/ui/CommandPanel.java
+++ src/br/com/irisbot/jarvis/ui/CommandPanel.java
@@ -1,46 +1,64 @@
 package br.com.irisbot.jarvis.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.bytedeco.javacpp.opencv_core.IplImage;
+
 import br.com.irisbot.jarvis.Main;
+import br.com.irisbot.jarvis.ipcam.IPCamCapture;
 import br.com.irisbot.jarvis.raspi.ServiceInstance;
 
 public class CommandPanel extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
 	
+	private ImagePane imgPane = new ImagePane(null); 
+	
 	private JButton[] buttons = new JButton[]{
-		new JButton("n√£o configurado"), new JButton("n√£o configurado"),
-		new JButton("n√£o configurado"), new JButton("n√£o configurado"),
-		new JButton("n√£o configurado"), new JButton("n√£o configurado"),
-		new JButton("n√£o configurado"), null,
-		new JButton("n√£o configurado") 
+		new JButton("n„o configurado"), new JButton("n„o configurado"),
+		new JButton("n„o configurado"), new JButton("n„o configurado"),
+		new JButton("n„o configurado"), new JButton("n„o configurado"),
+		new JButton("n„o configurado"), new JButton("n„o configurado"),
+		new JButton("n„o configurado") 
 	};
 	
 	public CommandPanel(){
 
-		this.setLayout(new GridLayout(3,3,10,10));
-		this.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+		this.setLayout(new BorderLayout());
+		this.add(imgPane, BorderLayout.CENTER);
+		
+		JPanel east = new JPanel();
+		
+		east.setLayout(new GridLayout(3,3,10,10));
+		east.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 		
 		for(JButton bt : buttons) {
 			if(bt!=null){
 				bt.setBackground(Color.LIGHT_GRAY);
 				bt.setBorder(BorderFactory.createRaisedBevelBorder());
-				this.add(bt);
+				east.add(bt);
 			}else{
-				this.add(new JLabel(""));
+				east.add(new JLabel(""));
 			}
 		}
 
-		buttons[0].setText("abrir port√£o");
+		this.add(east, BorderLayout.EAST);
+		
+		buttons[0].setText("abrir port„o");
 		buttons[0].setBackground(Color.CYAN);
 		buttons[0].addActionListener(new ActionListener() {
 			@Override
@@ -84,6 +102,48 @@ public class CommandPanel extends JPanel{
 				}
 			}
 		});
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				IPCamCapture cam = new IPCamCapture("rtsp://admin:123@192.168.0.100:554/onvif1");
+				//IPCamCapture cam = new IPCamCapture("rtsp://viewer:temp4now@192.168.0.191:554/videoMain");
+				cam.start();
+				while(true/*cam.isActive()*/){
+					try{Thread.sleep(1000/cam.getFrameRate());}catch (Exception e) {}
+					imgPane.setImage(cam.getImage());
+				}
+			}
+		}).start();
+		
+	}
+	
+	private static class ImagePane extends JPanel {
+		private static final long serialVersionUID = 1L;
+		private BufferedImage img;
+		private BufferedImage NULL; 
+
+		private ImagePane(BufferedImage img) {
+			try{
+				NULL = (BufferedImage) ImageIO.read(CommandPanel.class.getResource("img/ipcam.png"));
+			}catch (Exception e) {}
+			this.img = img;
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {
+			if(img==null) img = NULL;
+			Graphics2D graphics2d = (Graphics2D) g;
+			graphics2d.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), 0, 0, img.getWidth(), img.getHeight(),
+					null);
+			super.paintComponents(g);
+		}
+
+		public void setImage(BufferedImage img) {
+			this.img = img;
+			repaint();
+		}
+
 	}
 
 }
